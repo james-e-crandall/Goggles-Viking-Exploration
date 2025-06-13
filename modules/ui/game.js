371472642bc3ca07c4fc84da6d95b88434a32030
james-e-crandall <james.e.crandall@gmail.com>
2025-06-13 07:59:20 -0500

    initial working

---
+++ modules/ui/game.js
@@ -0,0 +1,13 @@
+window.UnitPortraitVisualRemaps = {
+    ...(window.UnitPortraitVisualRemaps || {}),
+    'UNIT_LONGBOAT': { //your unit type
+        unitType: 'UNIT_QUADRIREME',
+        background: "UnitPortraitsBG_UNIQUE", //"UnitPortraitsBG_UNIQUE" || "UnitPortraitsBG_BASE"
+    },
+}
+
+
+
+
+
+
