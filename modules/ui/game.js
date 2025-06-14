53348c1af9f9984a1e91796377f6599db61989d2
james-e-crandall <james.e.crandall@gmail.com>
2025-06-14 14:06:23 -0500

    almost working

--- modules/ui/game.js
+++ modules/ui/game.js
@@ -1,10 +1,19 @@
 window.UnitPortraitVisualRemaps = {
     ...(window.UnitPortraitVisualRemaps || {}),
     'UNIT_LONGBOAT': { //your unit type
-        unitType: 'UNIT_QUADRIREME',
+        unitType: 'UNIT_CETBANG',
+        background: "UnitPortraitsBG_UNIQUE", //"UnitPortraitsBG_UNIQUE" || "UnitPortraitsBG_BASE"
+    },
+    'UNIT_LONGBOAT': { //your unit type
+        unitType: 'UNIT_CETBANG_2',
+        background: "UnitPortraitsBG_UNIQUE", //"UnitPortraitsBG_UNIQUE" || "UnitPortraitsBG_BASE"
+    },
+    'UNIT_LONGBOAT': { //your unit type
+        unitType: 'UNIT_CETBANG_3',
         background: "UnitPortraitsBG_UNIQUE", //"UnitPortraitsBG_UNIQUE" || "UnitPortraitsBG_BASE"
     },
 }
+
 
 
 
